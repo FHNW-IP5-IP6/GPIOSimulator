bcaa8c0472efdc4579b0f0e261c9ea76acae6817
Jonathan Bättig <jonathan.baettig@students.fhnw.ch>
2020-03-17 10:25:04 +0100

    refactored project and added BlinkLed class

---
+++ src/main/java/pi4jexamples/BlinkLed.java
@@ -0,0 +1,31 @@
+package pi4jexamples;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.util.Console;
+
+public class BlinkLed {
+    public static void main(String[] args) throws Exception {
+        GpioFactory.setDefaultProvider(new RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING)); //like on GPIO Extension Board
+
+        final GpioController gpio = GpioFactory.getInstance();
+
+        GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_02, "Blinking LED" , PinState.LOW);
+        led.setShutdownOptions(true, PinState.LOW);
+
+        Console console = new Console();
+        console.promptForExit();
+
+        while (console.isRunning()) {
+            if (led.getState() == PinState.LOW) {
+                led.high();
+                console.println("Led is high.");
+            } else {
+                led.low();
+                console.println("Led is low.");
+            }
+            Thread.sleep(1000);
+        }
+
+        gpio.shutdown();
+    }
+}

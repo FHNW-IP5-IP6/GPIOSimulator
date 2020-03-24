4fd602dbf7d4721586a651863c40f06e31cafd14
Jonathan Bättig <jonathan.baettig@students.fhnw.ch>
2020-03-24 15:05:39 +0100

    added console output and gpio cleanup

--- src/main/java/pi4jexamples/MotorDirection.java
+++ src/main/java/pi4jexamples/MotorDirection.java
@@ -1,10 +1,12 @@
 package pi4jexamples;
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.util.Console;
 
 public class MotorDirection implements Example {
     private GpioPinDigitalOutput moveForward;
     private GpioPinDigitalOutput moveBackward;
+    private Console console;
 
     @Override public void execute() throws Exception {
         GpioFactory.setDefaultProvider(
@@ -13,7 +15,10 @@ public class MotorDirection implements Example {
         moveForward = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_17, "Motor Forward", PinState.LOW);
         moveBackward = gpio.provisionDigitalOutputPin(RaspiBcmPin.GPIO_27, "Motor Backward", PinState.LOW);
 
-        while (true) {
+        console = new Console();
+        console.promptForExit();
+
+        while (console.isRunning()) {
             motorForward();
             Thread.sleep(1000);
             motorHalt();
@@ -23,20 +28,25 @@ public class MotorDirection implements Example {
             motorHalt();
             Thread.sleep(1000);
         }
+
+        gpio.shutdown();
     }
 
     private void motorForward() {
         moveForward.high();
         moveBackward.low();
+        console.println("Motor is spinning forward.");
     }
 
     private void motorBackward() {
         moveForward.low();
         moveBackward.high();
+        console.println("Motor is spinning backward.");
     }
 
     private void motorHalt() {
         moveForward.low();
         moveBackward.low();
+        console.println("Motor is stopped.");
     }
 }

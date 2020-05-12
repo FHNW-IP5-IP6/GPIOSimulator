6009b998cb24a0c20299fb7ac146daa2de23e4f6
Jonathan Bättig <jonathan.baettig@students.fhnw.ch>
2020-05-12 11:49:41 +0200

    refactored project structure

--- src/main/java/FHNWExamples/Component/LcdSystemTimeDevice.java
+++ src/main/java/FHNWExamples/Component/LcdSystemTimeDevice.java
@@ -1,11 +1,8 @@
-package gpioexample.component;
+package FHNWExamples.Component;
 
+import com.pi4j.component.lcd.impl.GpioLcdDisplay;
 import com.pi4j.io.gpio.RaspiPin;
-import com.pi4j.wiringpi.Gpio;
-import com.pi4j.wiringpi.Lcd;
-import gpioexample.Example;
-import gpiosimulator.component.lcd.LCDTextAlignment;
-import gpiosimulator.component.lcd.impl.GpioLcdDisplay;
+import FHNWExamples.Example;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,25 +16,23 @@ public class LcdSystemTimeDevice extends Example {
         super(key, title);
     }
 
-
     @Override public void execute() throws Exception {
 
-        GpioLcdDisplay lcdDisplay = new GpioLcdDisplay(LCD_ROWS, LCD_COLUMNS,
-                RaspiPin.GPIO_06,      // LCD RS pin
+        GpioLcdDisplay lcdDisplay = new GpioLcdDisplay(LCD_ROWS, LCD_COLUMNS, RaspiPin.GPIO_06,      // LCD RS pin
                 RaspiPin.GPIO_05,      // LCD strobe pin
                 RaspiPin.GPIO_04,      // LCD data bit 0
                 RaspiPin.GPIO_00,      // LCD data bit 1
                 RaspiPin.GPIO_01,      // LCD data bit 2
                 RaspiPin.GPIO_03);
 
-        lcdDisplay.write(1,"GPIOSimulator", LCDTextAlignment.ALIGN_CENTER);
+        lcdDisplay.write(1, "GPIOSimulator");
 
         SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
 
         // update time every one second
         while (true) {
             // write time to line 2 on LCD
-            lcdDisplay.write(2,formatter.format(new Date()), LCDTextAlignment.ALIGN_CENTER);
+            lcdDisplay.write(2, formatter.format(new Date()));
             Thread.sleep(1000);
         }
     }

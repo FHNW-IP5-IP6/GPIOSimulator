83c3caeac6e086eef3f2e9118cb66c89c48c1a7f
Jonathan Bättig <jonathan.baettig@students.fhnw.ch>
2020-05-26 15:28:40 +0200

    refactored enum to use a constructor

--- src/main/java/FHNWGPIO/Grove/Adapter.java
+++ src/main/java/FHNWGPIO/Grove/Adapter.java
@@ -5,11 +5,21 @@ import com.pi4j.io.gpio.Pin;
 public class Adapter {
     private Pin upperPin;
     private Pin lowerPin;
+    private int analotI2CAddress;
+    private int upperDeviceAddress;
+    private int lowerDeviceAddress;
     private AdapterType type;
 
     protected Adapter(Pin upperPin, Pin lowerPin, AdapterType type) {
         this.upperPin = upperPin;
         this.lowerPin = lowerPin;
+        this.type = type;
+    }
+
+    protected Adapter(int analotI2CAddress, int upperDeviceAddress, int lowerDeviceAddress, AdapterType type) {
+        this.analotI2CAddress = analotI2CAddress;
+        this.upperDeviceAddress = upperDeviceAddress;
+        this.lowerDeviceAddress = lowerDeviceAddress;
         this.type = type;
     }
 
@@ -19,6 +29,18 @@ public class Adapter {
 
     public Pin getLowerPin() {
         return lowerPin;
+    }
+
+    private int getAnalotI2CAddress() {
+        return analotI2CAddress;
+    }
+
+    private int getUpperDeviceAddress() {
+        return upperDeviceAddress;
+    }
+
+    private int getLowerDeviceAddress() {
+        return lowerDeviceAddress;
     }
 
     public AdapterType getAdapterType() {
--- src/main/java/FHNWGPIO/Grove/GroveAdapter.java
+++ src/main/java/FHNWGPIO/Grove/GroveAdapter.java
@@ -9,8 +9,10 @@ import com.pi4j.io.gpio.RaspiBcmPin;
  * https://wiki.seeedstudio.com/Grove_Base_Hat_for_Raspberry_Pi/
  */
 public enum GroveAdapter {
-    // TODO: Handle Analog Pins
-    // TODO: Handle I2C Pins
+    A0(0x00, 0x01, AdapterType.ANALOG),
+    A2(0x02, 0x03, AdapterType.ANALOG),
+    A4(0x04, 0x05, AdapterType.ANALOG),
+    A6(0x05, 0x07, AdapterType.ANALOG),
     D5(RaspiBcmPin.GPIO_05, RaspiBcmPin.GPIO_06, AdapterType.DIGITAL),
     D16(RaspiBcmPin.GPIO_16, RaspiBcmPin.GPIO_17, AdapterType.DIGITAL),
     D18(RaspiBcmPin.GPIO_18, RaspiBcmPin.GPIO_19, AdapterType.DIGITAL),
@@ -22,9 +24,14 @@ public enum GroveAdapter {
     UART(RaspiBcmPin.GPIO_14, RaspiBcmPin.GPIO_15, AdapterType.UART);
 
     private Adapter adapter;
+    private int analogI2CBus = 0x04;
 
     GroveAdapter(Pin upperPin, Pin lowerPin, AdapterType type) {
         this.adapter = new Adapter(upperPin, lowerPin, type);
+    }
+
+    GroveAdapter(int upperDeviceAddress, int lowerDeviceAddress, AdapterType type) {
+        this.adapter = new Adapter(analogI2CBus, upperDeviceAddress, lowerDeviceAddress, type);
     }
 
     public Adapter getAdapter() {

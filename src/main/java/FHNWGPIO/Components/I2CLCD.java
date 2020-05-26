096174ba96a348f523af826edd76f9f211f8dcd1
Anessollah Ima <anessollah.ima@students.fhnw.ch>
2020-05-26 11:33:58 +0200

    added base i2c class and fixed some bugs

--- src/main/java/FHNWGPIO/Components/I2CLCD.java
+++ src/main/java/FHNWGPIO/Components/I2CLCD.java
@@ -1,6 +1,6 @@
 package FHNWGPIO.Components;
 
-import com.pi4j.io.i2c.I2CBus;
+import FHNWExamples.Component.Base.I2CBase;
 import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
 
@@ -10,19 +10,17 @@ import java.io.IOException;
  * I2C LCD Component for easier usage
  * Inspired by https://github.com/Poduzov/PI4J-I2C-LCD
  */
-public class I2CLCD {
+public class I2CLCD extends I2CBase {
 
     private final int ROWS = 2;
     private final int COLUMNS = 16;
-    private I2CDevice I2Cdevice;
 
     public I2CLCD(I2CDevice device) {
-        I2Cdevice = device;
+        super(device);
     }
 
     public I2CLCD(int address, int busNumber) throws IOException, I2CFactory.UnsupportedBusNumberException {
-        I2CBus bus = I2CFactory.getInstance(busNumber);
-        I2Cdevice = bus.getDevice(address);
+        super(address, busNumber);
     }
 
     /**
@@ -30,15 +28,15 @@ public class I2CLCD {
      */
     public void init() {
         try {
-            lcdWrite((byte) 0x03);
-            lcdWrite((byte) 0x03);
-            lcdWrite((byte) 0x03);
-            lcdWrite((byte) 0x02);
+            writeCommand((byte) 0x03);
+            writeCommand((byte) 0x03);
+            writeCommand((byte) 0x03);
+            writeCommand((byte) 0x02);
 
-            lcdWrite((byte) (LCD_FUNCTIONSET | LCD_2LINE | LCD_5x8DOTS | LCD_4BITMODE));
-            lcdWrite((byte) (LCD_DISPLAYCONTROL | LCD_DISPLAYON));
-            lcdWrite((byte) (LCD_CLEARDISPLAY));
-            lcdWrite((byte) (LCD_ENTRYMODESET | LCD_ENTRYLEFT));
+            writeCommand((byte) (LCD_FUNCTIONSET | LCD_2LINE | LCD_5x8DOTS | LCD_4BITMODE));
+            writeCommand((byte) (LCD_DISPLAYCONTROL | LCD_DISPLAYON));
+            writeCommand((byte) (LCD_CLEARDISPLAY));
+            writeCommand((byte) (LCD_ENTRYMODESET | LCD_ENTRYLEFT));
             Thread.sleep(0, 200000);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
@@ -85,6 +83,7 @@ public class I2CLCD {
 
         byte posNew = 0;
 
+        //lcd only has 2 lines, so I assume the first one if the second one wasn't selected explicitly
         if (line != 2) {
             posNew = (byte) pos;
             if (text.length() > COLUMNS - pos) {
@@ -100,7 +99,7 @@ public class I2CLCD {
     }
 
     /**
-     * shows text on display scrolls it with a delay
+     * shows text on display and scrolls it with a delay
      * @param text to display
      * @param line on which the text should be visible
      * @param delay for every position jump
@@ -118,7 +117,7 @@ public class I2CLCD {
                 clearLine(line);
 
             if (i == paddedtext.length() - 1 && startAgain)
-                displayScrollText(text, line, delay, startAgain, jumpToNextLine);
+                displayScrollText(text, line, delay, jumpToNextLine, startAgain);
         }
     }
 
@@ -133,10 +132,10 @@ public class I2CLCD {
      * @throws InterruptedException
      */
     public void displayBounceText(String text, int line, int delay, boolean jumpToNextLine, boolean startAgain) throws InterruptedException {
-        if (text.length() > COLUMNS - 1) {
+        if (text.length() >= COLUMNS - 1) { // Bounce doesn't make sense for a large text
             displayText(text, line);
         }
-
+        //shifts the whole text to the right end
         for (int i = 0; i < COLUMNS - text.length(); i++) {
             if (i + text.length() <= COLUMNS) {
                 displayText(text, line, i, jumpToNextLine);
@@ -147,6 +146,7 @@ public class I2CLCD {
                     clearLine(line);
             }
         }
+        //shifts the whole text to the left end
         for (int i = COLUMNS - text.length(); i > 0; i--) {
             displayText(text, line, i, jumpToNextLine);
             Thread.sleep(delay);
@@ -175,8 +175,8 @@ public class I2CLCD {
      * clears the lcd text
      */
     public void clearText() {
-        lcdWrite((byte) LCD_CLEARDISPLAY);
-        lcdWrite((byte) LCD_RETURNHOME);
+        writeCommand((byte) LCD_CLEARDISPLAY);
+        writeCommand((byte) LCD_RETURNHOME);
     }
 
     /**
@@ -185,20 +185,18 @@ public class I2CLCD {
      * @param pos for the start of the text
      */
     private void displayLine(String text, int pos) {
-        lcdWrite((byte) (0x80 + pos));
+        writeCommand((byte) (0x80 + pos));
 
         for (int i = 0; i < text.length(); i++) {
-            writeChar((byte) text.charAt(i), Rs);
+            writeCharacter((byte) text.charAt(i));
         }
     }
 
     /**
      * write a character to lcd
      */
-    public void writeChar(byte charvalue) {
-        byte mode = 1;
-        lcdWriteFourBits((byte) (mode | (charvalue & 0xF0)));
-        lcdWriteFourBits((byte) (mode | ((charvalue << 4) & 0xF0)));
+    public void writeCharacter(byte charvalue) {
+        writeSplitCommand(charvalue,Rs);
     }
 
     //Gets an empty line
@@ -207,129 +205,55 @@ public class I2CLCD {
         return String.format("%16s", text);
     }
 
-    //Clears a line
+    /**
+     * clears the selected lin
+     * @param line line to clear
+     */
     private void clearLine(int line) {
         displayText(getEmptyLine(), line);
     }
 
-    /**
-     * send a single command
-     *
-     * @param cmd command to send
-     */
-    private void writeCmd(byte cmd) {
-        try {
-            I2Cdevice.write(cmd);
-            Thread.sleep(0, 100000);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    /**
-     * Write Block of data
-     *
-     * @param cmd
-     * @param data
-     */
-    private void writeBlockData(byte cmd, byte[] data) {
-        try {
-            I2Cdevice.write(cmd, data);
-            Thread.sleep(0, 100000);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    /**
-     * Read a single byte
-     *
-     */
-    private byte read() {
-        try {
-            return (byte) I2Cdevice.read();
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        return (byte) 0;
-    }
-
-    /**
-     * Reads a byte array
-     */
-    private byte[] readData(byte cmd) {
-        byte[] buffer = new byte[cmd];
-        try {
-            I2Cdevice.read(buffer, 0, cmd);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        return buffer;
-    }
-
-    /**
-     * Reads a block of data
-     */
-    private byte[] readBlockData(byte cmd) {
-        byte[] buffer = new byte[cmd];
-        try {
-            I2Cdevice.read(buffer, 0, cmd);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-        return buffer;
-    }
 
     /**
      * clocks EN to latch command
      */
     private void lcdStrobe(byte data) {
         try {
-            I2Cdevice.write((byte) (data | En | LCD_BACKLIGHT));
+            writeCmd((byte) (data | En | LCD_BACKLIGHT));
             Thread.sleep(0, 500000);
-            I2Cdevice.write((byte) ((data & ~En) | LCD_BACKLIGHT));
+            writeCmd((byte) ((data & ~En) | LCD_BACKLIGHT));
             Thread.sleep(0, 100000);
         } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            getConsole().println(ex.getMessage());
         }
-    }
-
-    /**
-     * writes four bits
-     */
-    private void lcdWriteFourBits(byte data) {
-        try {
-            I2Cdevice.write((byte) (data | LCD_BACKLIGHT));
-            lcdStrobe(data);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    /**
-     * write characters
-     */
-    private void writeChar(byte cmd, byte mode) {
-        lcdWriteFourBits((byte) (mode | (cmd & 0xF0)));
-        lcdWriteFourBits((byte) (mode | ((cmd << 4) & 0xF0)));
     }
 
     /**
      * write a command to lcd
      */
-    private void lcdWrite(byte cmd) {
-        writeChar(cmd, (byte) 0);
+    private void writeCommand(byte cmd) {
+        writeSplitCommand(cmd, (byte) 0);
     }
 
     /**
-     * add custom characters(0 - 7)
+     * write byte split in two as it is initialized in 4Bit Mode
+     * commands larger than 4 bits should use this
      */
-    private void loadCustomChars(byte[][] fontdata) {
-        lcdWrite((byte) 0x40);
-        for (int i = 0; i < fontdata.length; i++) {
-            for (int j = 0; j < fontdata[i].length; j++) {
-                writeChar(fontdata[i][j]);
-            }
+    private void writeSplitCommand(byte cmd, byte mode) {
+        //bitwise AND with 11110000 to remove last 4 bits
+        writeFourBits((byte) (mode | (cmd & 0xF0)));
+        //bitshift and bitwise AND to remove first 4 bits
+        writeFourBits((byte) (mode | ((cmd << 4) & 0xF0)));
+    }
+    /**
+     * writes four bits
+     */
+    private void writeFourBits(byte data) {
+        try {
+            writeCmd((byte) (data | LCD_BACKLIGHT));
+            lcdStrobe(data);
+        } catch (Exception ex) {
+            getConsole().println(ex.getMessage());
         }
     }
 

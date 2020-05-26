63b696232b4eebca4e52529e4ca38c16018a8a3d
Jonathan Bättig <jonathan.baettig@students.fhnw.ch>
2020-05-26 11:26:06 +0200

    refactored enum to use a constructor

--- src/main/java/FHNWGPIO/Grove/GroveAdapter.java
+++ src/main/java/FHNWGPIO/Grove/GroveAdapter.java
@@ -1,63 +1,33 @@
 package FHNWGPIO.Grove;
 
+import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.RaspiBcmPin;
 
-// https://wiki.seeedstudio.com/Grove_Base_Hat_for_Raspberry_Pi/
-// Board only supports 3.3V Grove Devices
+/**
+ * This enum provides Adapter objects for programming the adaptors of the grove hat.
+ * Attention! Board only supports 3.3V Grove Devices!
+ * https://wiki.seeedstudio.com/Grove_Base_Hat_for_Raspberry_Pi/
+ */
 public enum GroveAdapter {
-    /*A0 {
-        @Override public Adapter getAdapter() {
-            return new Adapter();
-        }
-    }, A2 {
-        @Override public Adapter getAdapter() {
-            return new Adapter();
-        }
-    }, A4 {
-        @Override public Adapter getAdapter() {
-            return new Adapter();
-        }
-    }, A6 {
-        @Override public Adapter getAdapter() {
-            return new Adapter();
-        }
-    },*/ D5 {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_05, RaspiBcmPin.GPIO_06, AdapterType.DIGITAL);
-        }
-    }, D16 {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_16, RaspiBcmPin.GPIO_17, AdapterType.DIGITAL);
-        }
-    }, D18 {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_18, RaspiBcmPin.GPIO_19, AdapterType.DIGITAL);
-        }
-    }, D22 {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_22, RaspiBcmPin.GPIO_23, AdapterType.DIGITAL);
-        }
-    }, D24 {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_24, RaspiBcmPin.GPIO_25, AdapterType.DIGITAL);
-        }
-    }, D26 {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_26, RaspiBcmPin.GPIO_27, AdapterType.DIGITAL);
-        }
-    }, I2C {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_02, RaspiBcmPin.GPIO_03, AdapterType.I2C);
-        }
-    }, PWM {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_12, RaspiBcmPin.GPIO_13, AdapterType.PWM);
-        }
-    }, UART {
-        @Override public Adapter getAdapter() {
-            return new Adapter(RaspiBcmPin.GPIO_14, RaspiBcmPin.GPIO_15, AdapterType.UART);
-        }
-    };
+    // TODO: Handle Analog Pins
+    // TODO: Handle I2C Pins
+    D5(RaspiBcmPin.GPIO_05, RaspiBcmPin.GPIO_06, AdapterType.DIGITAL),
+    D16(RaspiBcmPin.GPIO_16, RaspiBcmPin.GPIO_17, AdapterType.DIGITAL),
+    D18(RaspiBcmPin.GPIO_18, RaspiBcmPin.GPIO_19, AdapterType.DIGITAL),
+    D22(RaspiBcmPin.GPIO_22, RaspiBcmPin.GPIO_23, AdapterType.DIGITAL),
+    D24(RaspiBcmPin.GPIO_24, RaspiBcmPin.GPIO_25, AdapterType.DIGITAL),
+    D26(RaspiBcmPin.GPIO_26, RaspiBcmPin.GPIO_27, AdapterType.DIGITAL),
+    // I2C(RaspiBcmPin.GPIO_02, RaspiBcmPin.GPIO_03, AdapterType.I2C),
+    PWM(RaspiBcmPin.GPIO_12, RaspiBcmPin.GPIO_13, AdapterType.PWM),
+    UART(RaspiBcmPin.GPIO_14, RaspiBcmPin.GPIO_15, AdapterType.UART);
 
-    public abstract Adapter getAdapter();
+    private Adapter adapter;
+
+    GroveAdapter(Pin upperPin, Pin lowerPin, AdapterType type) {
+        this.adapter = new Adapter(upperPin, lowerPin, type);
+    }
+
+    public Adapter getAdapter() {
+        return adapter;
+    }
 }

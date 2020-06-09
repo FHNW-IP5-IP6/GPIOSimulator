459ef360e94ed664722efd072e02b0eccd12e364
Anessollah Ima <anessollah.ima@students.fhnw.ch>
2020-06-09 11:27:59 +0200

    finalized RaspberryPiCamera Example

--- src/main/java/FHNWExamples/Component/RaspberryPiCamera.java
+++ src/main/java/FHNWExamples/Component/RaspberryPiCamera.java
@@ -1,21 +1,27 @@
 package FHNWExamples.Component;
 
 import FHNWExamples.Example;
+
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.pi4j.util.Console;
 import uk.co.caprica.picam.*;
 import uk.co.caprica.picam.enums.*;
+
 import static uk.co.caprica.picam.CameraConfiguration.cameraConfiguration;
 import static uk.co.caprica.picam.PicamNativeLibrary.installTempLibrary;
 
 /**
- * This is an example of the raspberry pi camera using a java library.
- *
+ * This is an example of the raspberry pi camera using a java library:
+ * https://github.com/caprica/picam
+ * <p>
  * The raspberry pi camera has to be enabled first in the raspberry pi configuration:
  * sudo raspi-config -> Interfacing Options -> Camera
- *
- * For additional information on how to take pictures or videos from the console with the Camera go to:
+ * <p>
+ * For additional information on how to take pictures or videos from the console with the camera go to:
  * https://www.raspberrypi.org/documentation/configuration/camera.md
- *
  */
 public class RaspberryPiCamera extends Example {
 
@@ -24,22 +30,41 @@ public class RaspberryPiCamera extends Example {
     }
 
     @Override
+    // tag::RaspberryPiCamera[]
     public void execute() throws Exception {
+        Console console = new Console();
+        console.promptForExit();
+
         // Extract the bundled picam native library to a temporary file and load it
         installTempLibrary();
 
+        //Create Configuration object with builder approach
         CameraConfiguration config = cameraConfiguration()
                 .width(1920)
                 .height(1080)
                 .encoding(Encoding.JPEG)
                 .quality(85);
 
+        //select default home directory for pictures
+        String path = "/home/pi/Pictures/PiCamPics";
+        Files.createDirectories(Paths.get(path));
+
+        console.println("tries to take 10 pictures to the directory" + path);
+
+        //takes picture to the added jpg file
         try (Camera camera = new Camera(config)) {
-            camera.takePicture(new FilePictureCaptureHandler(new File("picam1.jpg")));
-            camera.takePicture(new FilePictureCaptureHandler(new File("picam2.jpg")));
-        }
-        catch (CameraException e) {
-            e.printStackTrace();
+            //For the first picture a longer delay is sometimes needed
+            camera.takePicture(new FilePictureCaptureHandler(new File(path + "/picam1.jpg")), 3000);
+            console.println("picture 1 successfully taken");
+
+            //takes 9 pictures quicker now to get 10 in total
+            for (int i = 2; i <= 10; i++) {
+                camera.takePicture(new FilePictureCaptureHandler(new File(path + "/picam" + i + ".jpg")));
+                console.println("picture " + i + " successfully taken");
+            }
+        } catch (CameraException e) {
+            console.println(e.getMessage());
         }
     }
+    // end::RaspberryPiCamera[]
 }
--- src/main/java/FHNWExamples/Examples.java
+++ src/main/java/FHNWExamples/Examples.java
@@ -35,26 +35,26 @@ public class Examples {
         int input = getNextScannerInt();
 
         switch (input) {
-        case 0:
-            System.exit(0);
-            break;
-        case 1:
-            //GPIO Examples
-            System.out.println("CHOOSE A GPIO EXAMPLE");
-            System.out.println("--------------------");
-            showCommandInformation(gpioExamples);
-            chooseExample(gpioExamples);
-            break;
-        case 2:
-            //GPIO Examples using pi4j-Component
-            System.out.println("CHOOSE A DEVICE EXAMPLE");
-            System.out.println("--------------------");
-            showCommandInformation(deviceExamples);
-            chooseExample(deviceExamples);
-            break;
-        default:
-            System.out.println("Did not recognize input");
-            showInitialInformation();
+            case 0:
+                System.exit(0);
+                break;
+            case 1:
+                //GPIO Examples
+                System.out.println("CHOOSE A GPIO EXAMPLE");
+                System.out.println("--------------------");
+                showCommandInformation(gpioExamples);
+                chooseExample(gpioExamples);
+                break;
+            case 2:
+                //GPIO Examples using pi4j-Component
+                System.out.println("CHOOSE A DEVICE EXAMPLE");
+                System.out.println("--------------------");
+                showCommandInformation(deviceExamples);
+                chooseExample(deviceExamples);
+                break;
+            default:
+                System.out.println("Did not recognize input");
+                showInitialInformation();
         }
     }
 
@@ -115,10 +115,11 @@ public class Examples {
      */
     private static void initializeDeviceExamples() {
         deviceExamples.add(new BlinkLedDevice(1, "Blink LED Test"));
-        deviceExamples.add(new ButtonClickDevice(2,"Button Click Test using pi4j-device"));
-        deviceExamples.add(new FHNWExamples.Component.LcdSystemTimeI2C(3,"LCD System Time I2C Test"));
-        deviceExamples.add(new FHNWExamples.Component.SerialCamera(4,"Grove Serial Camera example"));
-        deviceExamples.add(new FHNWExamples.Component.PotentiometerDevice(5,"Potentiometer example"));
+        deviceExamples.add(new ButtonClickDevice(2, "Button Click Test using pi4j-device"));
+        deviceExamples.add(new FHNWExamples.Component.LcdSystemTimeI2C(3, "LCD System Time I2C Test"));
+        deviceExamples.add(new FHNWExamples.Component.SerialCamera(4, "Grove Serial Camera example"));
+        deviceExamples.add(new FHNWExamples.Component.PotentiometerDevice(5, "Potentiometer example"));
+        deviceExamples.add(new FHNWExamples.Component.RaspberryPiCamera(6, "Raspberry Pi Camera example"));
     }
 
     /**

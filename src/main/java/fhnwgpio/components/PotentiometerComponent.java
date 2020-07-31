642d54167ed0d57b592c774e413d2a9cd7977538
Jonathan Bättig <jonathan.baettig@students.fhnw.ch>
2020-07-31 15:56:04 +0200

    added potentiometer to advanced information

--- src/main/java/fhnwgpio/components/PotentiometerComponent.java
+++ src/main/java/fhnwgpio/components/PotentiometerComponent.java
@@ -13,67 +13,78 @@ import java.io.IOException;
  */
 public class PotentiometerComponent extends I2CBase {
     private int minValue = 0;
-    private int maxValue = 999;
+    private int maxValue = 1000;
     private Adapter adapter;
 
     /**
      * Constructor of the PotentiometerComponent with Grove adapter.
      *
-     * @param groveAdapter  The Grove adapter where the potentiometer ist connected.
+     * @param groveAdapter The Grove adapter where the potentiometer ist connected.
      * @throws IOException
      * @throws I2CFactory.UnsupportedBusNumberException
      */
+    // tag::PotentiometerComponentConstructor[]
     public PotentiometerComponent(GroveAdapter groveAdapter)
             throws IOException, I2CFactory.UnsupportedBusNumberException {
         super(groveAdapter.getAdapter().getAnalogI2CAddress(), I2CBus.BUS_1);
         this.adapter = groveAdapter.getAdapter();
     }
+    // end::PotentiometerComponentConstructor[]
 
     /**
      * Set the value returned by getValue() to a specific range. The difference between minValue and MaxValue must be
-     * between 10 and 999.
+     * between 9 and 1001.
      *
-     * @param minValue  The desired minimum value returned.
-     * @param maxValue  The desired maximum value returned.
+     * @param minValue The desired minimum value returned. Needs to be greater than 0.
+     * @param maxValue The desired maximum value returned. Needs to be greater than 0.
+     * @throws IllegalArgumentException Thrown if minValue or maxValue is a negative number or its difference is not between 9 and 1001.
      */
-    public void setRange(int minValue, int maxValue) {
+    // tag::PotentiometerComponentSetRange[]
+    public void setRange(int minValue, int maxValue) throws IllegalArgumentException {
+        if (minValue < 0 || maxValue < 0) {
+            throw new IllegalArgumentException("minValue and maxValue must be greater than 0");
+        }
+
         int difference = maxValue - minValue;
-        if (difference < 10 || difference > 999) {
+        if (difference < 10 || difference > 1000) {
             throw new IllegalArgumentException(
-                    "the difference between min and max value needs to be between 10 and 999");
+                    "the difference between min and max value needs to be between 10 and 1000");
         }
 
         this.minValue = minValue;
         this.maxValue = maxValue;
     }
+    // end::PotentiometerComponentSetRange[]
 
     /**
      * Get the current value from the potentiometer.
      *
-     * @return  The current value of the potentiometer adjusted to the desired range.
+     * @return The current value of the potentiometer adjusted to the desired range.
      */
+    // tag::PotentiometerComponentGetValue[]
     public int getValue() {
         byte[] bytes = readData((byte) adapter.getDeviceAddress(), (byte) 2);
         int readValue = getIntegerFromBytes(bytes[0], bytes[1]);
-        if (minValue == 0 && maxValue == 999) {
+        if (minValue == 0 && maxValue == 1000) {
             return readValue;
         } else {
             int difference = maxValue - minValue;
             if (readValue == 0) {
-                return readValue;
+                return minValue;
             } else {
                 double reducedValue = ((double) readValue / 1000) * difference;
                 return (int) Math.round(minValue + reducedValue);
             }
         }
     }
+    // end::PotentiometerComponentGetValue[]
 
     /**
      * Converts a byte into an integer.
      *
      * @param low  Low byte
      * @param high High byte
-     * @return  Integer value of the bytes.
+     * @return Integer value of the bytes.
      */
     private static int getIntegerFromBytes(byte low, byte high) {
         return (0xff & (byte) 0x00) << 24 | (0xff & (byte) 0x00) << 16 | (0xff & high) << 8 | (0xff & low) << 0;

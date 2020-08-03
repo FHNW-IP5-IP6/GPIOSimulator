826ab210e1954bd05606b1dde0dd9b2875d2315f
Anessollah Ima <anessollah.ima@students.fhnw.ch>
2020-08-03 10:59:45 +0200

    improved javadoc comments and added tags to RFID Reader

--- src/main/java/fhnwexamples/component/RFIDReader.java
+++ src/main/java/fhnwexamples/component/RFIDReader.java
@@ -7,7 +7,6 @@ import com.pi4j.io.serial.*;
 import com.pi4j.util.Console;
 import fhnwexamples.Example;
 import fhnwgpio.components.RFIDReaderComponent;
-
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -18,25 +17,24 @@ public class RFIDReader extends Example {
     }
 
     @Override
-    public void execute() throws Exception {
+    public void execute(){
         GpioFactory.setDefaultProvider(
                 new RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING)); //like on gpio Extension Board
         Console console =  new Console();
         console.promptForExit();
 
+        // tag::RFIDReader[]
         RFIDReaderComponent rfidReaderComponent = new RFIDReaderComponent(console);
         // create and register the serial data listener
-        rfidReaderComponent.addListener(new SerialDataEventListener() {
-            @Override
-            public void dataReceived(SerialDataEvent event) {
-                // print out the data received to the console
-                try {
-                    console.println("[HEX DATA]   " + event.getHexByteString());
-                    console.println("[ASCII DATA] " + event.getAsciiString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        rfidReaderComponent.addListener(event -> {
+            // print out the data received to the console
+            try {
+                console.println("[HEX DATA]   " + event.getHexByteString());
+                console.println("[ASCII DATA] " + event.getAsciiString());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         });
+        // end::RFIDReader[]
     }
 }
--- src/main/java/fhnwgpio/components/RFIDReaderComponent.java
+++ src/main/java/fhnwgpio/components/RFIDReaderComponent.java
@@ -2,7 +2,6 @@ package fhnwgpio.components;
 
 import com.pi4j.io.serial.*;
 import com.pi4j.util.Console;
-
 import java.io.IOException;
 
 /**
@@ -11,34 +10,44 @@ import java.io.IOException;
  */
 public class RFIDReaderComponent {
     private Console console;
-    private Serial serial = null;
+    private Serial serial;
 
-    public RFIDReaderComponent(Console console) throws IOException, InterruptedException {
-
+    /**
+     * Constructor of the RFID Reader
+     * @param console Pi4J Console object for loggin
+     */
+    public RFIDReaderComponent(Console console){
         this.console = console;
         serial = SerialFactory.createInstance();
         init();
-
     }
 
+    /**
+     * Adds Listeners when the for when the device receives data
+     * @param listener listeners to add
+     */
     public void addListener(SerialDataEventListener... listener) {
         serial.addListener(listener);
-        this.console.println("Event Listener " + listener.toString() + " added");
+        this.console.println("Event Listener for added for received data");
     }
 
-    private void init() throws IOException, InterruptedException {
+    /**
+     * Initialises the serial port of the device
+     */
+    private void init() {
         try {
             SerialConfig config = new SerialConfig();
             config.device(SerialPort.getDefaultPort())
                     .baud(Baud._9600)
                     .dataBits(DataBits._8)
                     .stopBits(StopBits._1);
+
             // display connection details
             console.box(" Connecting to: " + config.toString(),
                     " Testing the RFID Reader");
             serial.open(config);
         } catch (Exception ex) {
-            this.console.println(" ==>> SERIAL SETUP FAILED : " + ex.getMessage());
+            this.console.println(" ==> SERIAL SETUP FAILED : " + ex.getMessage());
         }
     }
 }

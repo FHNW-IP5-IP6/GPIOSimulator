0c252afc13afaa92a26aa32c81a04e47169875f2
Jonathan Bättig <jonathan.baettig@students.fhnw.ch>
2020-08-07 17:48:04 +0200

    added PwmHelper and buzzer documentation

---
+++ src/main/java/FHNWGPIO/Components/BuzzerComponent.java
@@ -0,0 +1,120 @@
+package fhnwgpio.components;
+
+import fhnwgpio.components.helper.PwmHelper;
+import fhnwgpio.grove.AdapterType;
+import fhnwgpio.grove.GroveAdapter;
+import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Gpio;
+
+/**
+ * FHNW implementation for the grove buzzer.
+ * https://www.seeedstudio.com/Grove-Buzzer.html
+ */
+public class BuzzerComponent {
+    private Pin pin;
+    private int piFrequency = 19200000;
+    private int dutyCycle = 512;
+
+    /**
+     * Private master constructor. All public constructors call this constructor.
+     *
+     * @param pin GPIO PWM pin
+     */
+    private BuzzerComponent(Pin pin) {
+        this.pin = pin;
+        Gpio.pinMode(this.pin.getAddress(), Gpio.PWM_OUTPUT);
+        Gpio.pwmSetMode(Gpio.PWM_MODE_MS);
+        Gpio.pwmSetRange(dutyCycle);
+    }
+
+    /**
+     * Constructor for GPIO buzzer usage
+     *
+     * @param pin Pi4J GPIO PWM output pin
+     * @throws IllegalArgumentException Thrown if the provided pin is a non hardware pwm pin
+     */
+    public BuzzerComponent(GpioPinPwmOutput pin) throws IllegalArgumentException {
+        this(pin.getPin());
+
+        if (!PwmHelper.isHardwarePwmPin(pin.getPin())) {
+            throw new IllegalArgumentException("please provide a valid pwm pin");
+        }
+    }
+
+    /**
+     * Constructor for Grove buzzer usage
+     *
+     * @param groveAdapter The PWM Grove adapter
+     * @throws IllegalArgumentException Thrown if the provided grove adapter is not the PWM adapter
+     */
+    public BuzzerComponent(GroveAdapter groveAdapter) throws IllegalArgumentException {
+        this(groveAdapter.getAdapter().getUpperPin());
+
+        if (groveAdapter.getAdapter().getAdapterType() != AdapterType.PWM) {
+            throw new IllegalArgumentException("please provide a grove pwm pin");
+        }
+    }
+
+    /**
+     * Plays a tone at a frequency for a specified duration in milliseconds.
+     *
+     * @param frequency in hz
+     * @param duration  in ms
+     * @throws InterruptedException Might be thrown because of Thread.sleep() usage
+     */
+    // tag::BuzzerComponentPlayTone[]
+    public void playTone(int frequency, int duration) throws InterruptedException {
+        if (frequency != 0) {
+            int clock = calculateClock(frequency);
+            Gpio.pwmSetClock(clock);
+            Gpio.pwmWrite(pin.getAddress(), clock);
+            Thread.sleep(duration);
+        } else {
+            stop(duration);
+        }
+    }
+    // end::BuzzerComponentPlayTone[]
+
+    /**
+     * Plays a tone with a specific frequency.
+     *
+     * @param frequency in hz
+     * @throws InterruptedException Might be thrown because of Thread.sleep() usage
+     */
+    public void playTone(int frequency) throws InterruptedException {
+        playTone(frequency, 0);
+    }
+
+    /**
+     * Pauses for a specified amount of milliseconds.
+     *
+     * @param duration Desired pause length in ms
+     * @throws InterruptedException Might be thrown because of Thread.sleep() usage
+     */
+    public void stop(int duration) throws InterruptedException {
+        Gpio.pwmWrite(pin.getAddress(), 0);
+        Thread.sleep(duration);
+    }
+
+    /**
+     * Pauses forever.
+     *
+     * @throws InterruptedException Might be thrown because of Thread.sleep() usage
+     */
+    public void stop() throws InterruptedException {
+        stop(0);
+    }
+
+    /**
+     * Calculates the PWM clock divider.
+     *
+     * @param frequency Desired frequency
+     * @return The calculated clock divider
+     */
+    // tag::BuzzerComponentCalculateClock[]
+    private int calculateClock(int frequency) {
+        double divider = (double) piFrequency / frequency;
+        return (int) divider / dutyCycle;
+    }
+    // end::BuzzerComponentCalculateClock[]
+}
--- src/main/java/fhnwexamples/component/BuzzerDevice.java
+++ src/main/java/fhnwexamples/component/BuzzerDevice.java
@@ -1,14 +1,13 @@
 package fhnwexamples.component;
 
+import com.pi4j.io.gpio.*;
 import fhnwexamples.Example;
 import fhnwgpio.components.BuzzerComponent;
 import fhnwgpio.components.helper.Note;
-import fhnwgpio.grove.GroveAdapter;
 import com.pi4j.util.Console;
 
 /**
- * Example for BuzzerComponent usage with a grove Buzzer. If the buzzer is connected to the PWM port of the grove Hat
- * it will play the Imperial March by John Williams.
+ * Example for BuzzerComponent usage with a GPIO buzzer. The buzzer will play the Imperial March by John Williams.
  * https://gist.github.com/StevenNunez/6786124
  */
 public class BuzzerDevice extends Example {
@@ -17,11 +16,16 @@ public class BuzzerDevice extends Example {
         super(key, title);
     }
 
+    // tag::BuzzerDevice[]
     @Override public void execute() throws Exception {
+        GpioFactory.setDefaultProvider(new RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING));
+        final GpioController gpio = GpioFactory.getInstance();
+
         Console console = new Console();
         console.promptForExit();
 
-        BuzzerComponent buzzer = new BuzzerComponent(GroveAdapter.PWM);
+        GpioPinPwmOutput pwm = gpio.provisionPwmOutputPin(RaspiBcmPin.GPIO_12);
+        BuzzerComponent buzzer = new BuzzerComponent(pwm);
 
         Note notes[] = { Note.G4, Note.G4, Note.G4, Note.DS4, Note.AS4, Note.G4, Note.DS4, Note.AS4, Note.G4, Note.D5,
                 Note.D5, Note.D5, Note.DS5, Note.AS4, Note.FS4, Note.DS4, Note.AS4, Note.G4, Note.G5, Note.G4, Note.G4,
@@ -34,16 +38,15 @@ public class BuzzerDevice extends Example {
 
         while (console.isRunning()) {
             for (int i = 0; i < notes.length; i++) {
-                if (notes[i] != Note.PAUSE) {
-                    buzzer.playTone(notes[i].getFrequency(), tempo * beats[i]);
-                } else {
-                    buzzer.stop(tempo * beats[i]);
-                }
+                buzzer.playTone(notes[i].getFrequency(), tempo * beats[i]);
             }
 
+            buzzer.stop();
             Thread.sleep(3000);
+
         }
 
         buzzer.stop();
     }
+    // end::BuzzerDevice[]
 }
